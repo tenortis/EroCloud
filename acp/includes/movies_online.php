74ec9ffa6f3613a0775fa01f256acfa30b5a0b22
Martin <info@erocms.net>
2026-07-01 00:15:53 +0200

    feat(acp): display total storage space of online movies in Filme-online page with caching

--- acp/includes/movies_online.php
+++ acp/includes/movies_online.php
@@ -9,15 +9,61 @@
 if (!defined('SAFE_INC'))
     die ("Hacking attempt...");
 
+if (!function_exists('cleanup_get_folder_size')) {
+    function cleanup_get_folder_size($folderPath) {
+        $size = 0;
+        if (!is_dir($folderPath)) {
+            return 0;
+        }
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folderPath, FilesystemIterator::SKIP_DOTS));
+        foreach ($iterator as $file) {
+            $size += $file->getSize();
+        }
+        return $size;
+    }
+}
+
+if (isset($_GET['recalc_size']) && $_GET['recalc_size'] == 1) {
+    $cache_file = sys_get_temp_dir() . '/erocloud_online_movies_size_cache.txt';
+    if (file_exists($cache_file)) {
+        unlink($cache_file);
+    }
+    header('Location: ' . ACP_URL . '/Filme-online');
+    exit;
+}
+
+$cache_file = sys_get_temp_dir() . '/erocloud_online_movies_size_cache.txt';
+$cache_lifetime = 86400; // 24 hours cache
+if (file_exists($cache_file) && (time() - filemtime($cache_file) < $cache_lifetime)) {
+    $total_online_size = (float)file_get_contents($cache_file);
+} else {
+    $total_online_size = 0;
+    $rs_online = p4c_query("SELECT m.id, m.merchant_id, m.storage_location FROM movies_online mo JOIN movies m ON mo.file_id = m.file_id;", __FILE__, __LINE__);
+    while ($m_row = p4c_fetch_object($rs_online)) {
+        $folder = MOVIES_PATH.'/'.$m_row->storage_location.'/'.$m_row->merchant_id.'/'.$m_row->id;
+        $total_online_size += cleanup_get_folder_size($folder);
+    }
+    file_put_contents($cache_file, $total_online_size);
+}
+
+if ($total_online_size >= 1099511627776) {
+    $total_online_size_text = number_format($total_online_size / 1099511627776, 2) . ' TB';
+} elseif ($total_online_size >= 1073741824) {
+    $total_online_size_text = number_format($total_online_size / 1073741824, 2) . ' GB';
+} else {
+    $total_online_size_text = number_format($total_online_size / 1048576, 2) . ' MB';
+}
+
 $site .= '
 <div id="tabs">
     <ul>
         <li><a href="#a">Filme &Uuml;bersicht</a></li>
     </ul>
     <div id="a">
-        <div class="ui-widget-header" style="padding:10px; font-size:20px;">'.$count_movies_online.' Filme online</div>
+        <div class="ui-widget-header" style="padding:10px; font-size:20px;">'.$count_movies_online.' Filme online (Speicherplatz: '.$total_online_size_text.')</div>
         <div class="ui-widget-content" style="padding:10px; border-top:none; margin-bottom:20px;">
-            Hier sind alle Filme aufgelistet die in der Cloud ver&ouml;ffentlicht sind. 
+            Hier sind alle Filme aufgelistet die in der Cloud ver&ouml;ffentlicht sind.
+            <a href="?recalc_size=1" style="font-size:11px; margin-left: 15px; color: #1c94c4; text-decoration: underline;">[Gr&ouml;&szlig;e neu berechnen]</a>
         </div>
         
         <script type="text/javascript">
